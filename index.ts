9feebb2eab2cca64e4eb37a542fe531ff12bd77d
MNB <buyskih@gmail.com>
2016-08-15 12:20:19 +0300

    Master commit

---
+++ index.ts
@@ -0,0 +1,40 @@
+module TSHelpers {
+    import IComponentOptions = angular.IComponentOptions;
+    export function Component(options: IComponentOptions): Function {
+        return (controller: Function) => {
+            if (controller.prototype.$componentOptions)
+                angular.extend(options, controller.prototype.$componentOptions);
+
+            if (!options.controllerAs) options.controllerAs = 'vm';
+
+            return angular.extend(options, {controller});
+        }
+    }
+
+    export function Require(source: string) {
+        return function (obj, name) {
+            addProperty(obj, name, source, 'require');
+        }
+    }
+
+    export function Input(direction: string = '<') {
+        return function (obj, name) {
+            addProperty(obj, name, direction);
+        }
+    }
+
+    export function Output(direction: string = '&') {
+        return Input(direction);
+    }
+
+    export interface $OnInit {
+        $onInit(): void;
+    }
+
+    function addProperty(obj, property, value, core = 'bindings') {
+        if (!obj['$componentOptions']) obj['$componentOptions'] = {};
+        if (!obj['$componentOptions'][core]) obj['$componentOptions'][core] = {};
+        obj['$componentOptions'][core][property] = value;
+        return obj;
+    }
+}
